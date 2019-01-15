a538752677df8f0542a3581e431dec817910f0b4
jbosegre <jbose@mail.greenriver.edu>
2019-01-14 22:17:27 -0500

    added CountBean javacript program

--- review-js.js
+++ review-js.js
@@ -22,3 +22,16 @@ function FizzBuzz(){
         }
     }
 }
+
+function countBs(string) {
+    var count = 0;
+    for (var i = 0; i < string.length; i++) {
+        if (string.charAt(i) === "B") {
+            count++;
+        }
+    }
+    return (count);
+}
+
+
+
