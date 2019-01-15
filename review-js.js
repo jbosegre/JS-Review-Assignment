3df1155bde6ed27a1d6ad994a939c71b8da714c2
jbosegre <jbose@mail.greenriver.edu>
2019-01-14 22:25:15 -0500

    added CountChar javacript program

--- review-js.js
+++ review-js.js
@@ -33,5 +33,17 @@ function countBs(string) {
     return (count);
 }
 
+function countChar(str, char) {
+    var count = 0;
+    for (var i = 0; i < str.length; i++) {
+        if (str.charAt(i) === "char") {
+            count++;
+            return (count);
+        }
+    }
+}
+
+
+
 
 
