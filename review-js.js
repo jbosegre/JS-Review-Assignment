dbd1194f40235beb7a146c0bcff1cf04632613a8
jbosegre <jbose@mail.greenriver.edu>
2019-01-14 22:08:56 -0500

    added fizzbuzz javacript program

--- review-js.js
+++ review-js.js
@@ -1,3 +1,24 @@
+var $countNumber = 0;
+var $maxNum = 100;
+
 function intro() {
     console.log("Introduction to JavaScript");
+}
+
+function FizzBuzz(){
+    for (var $i = 0; $i < $maxNum; $i++){
+        if($i % 3 && !$i % 5){
+            console.log("Fizz");
+        }
+        else if($i % 5 && !$i % 3){
+            console.log("Buzz");
+        }
+        else if ($i % 3 && $i % 5){
+            console.log("FizzBuzz");
+        }
+        else{
+            $countNumber += 1;
+            console.log($countNumber);
+        }
+    }
 }
